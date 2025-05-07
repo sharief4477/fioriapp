72f45849b0afe6268b93e69faac20c4ef58a9c06
Sharief Ahamed <shariefahamed@sbpcorp.in>
2025-05-07 13:38:40 +0000

    first

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("cpapp.fiori.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
